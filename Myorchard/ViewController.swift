0a134ceed4657c7be5d9ea2eaf663c484b152829
Naphisaporn <pewdog30@hotmail.com>
2020-01-23 21:59:11 +0700

    Initial Commit

---
+++ Myorchard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Myorchard
+//
+//  Created by PewPew on 23/1/2563 BE.
+//  Copyright © 2563 PewPew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyorchardTests/MyorchardTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyorchardTests.swift
+//  MyorchardTests
+//
+//  Created by PewPew on 23/1/2563 BE.
+//  Copyright © 2563 PewPew. All rights reserved.
+//
+
+import XCTest
+@testable import Myorchard
+
+class MyorchardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
